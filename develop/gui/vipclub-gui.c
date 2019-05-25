b52154b3b95f4d06e3dfd67b09fd720e1be0cb65
Chartwalker <bastelmaus@chartwalker.net>
2019-05-25 14:35:08 +0200

    added hardware support log-file

--- develop/gui/vipclub-gui.c
+++ develop/gui/vipclub-gui.c
@@ -1,44 +1,80 @@
-#include <gtk/gtk.h>
 #include "vipclub-gui.h"
+#include <glib.h>
 
-GtkWidget *window,*table,*entry;
+#include <gtk/gtk.h>
 
-GdkPixbuf *about_logo;
+ gint color;
 
-about_logo = gdk_pixbuf_new_from_file ("./logo.png", NULL);
-gtk_show_about_dialog (NULL,
-                       "VIP-Club", "Code",
-                       "logo", about_logo,
-                       "title", _("About Code"),
-                       NULL);
+ GtkWidget *image[4];
+ GtkWidget *window;
+ GtkWidget *list;
+ GtkWidget *button;
 
-/* initialize widget pointers */
+ gint color = 0;
+ gint color_max=3;
+ gint color_min=1;
 
-table = gtk_table_new(5, 4, TRUE);
-entry = gtk_entry_new();
-about_logo = gdk_pixbuf_new_from_file ("./logo.png", NULL);
-
-/* packing for take off */
-gtk_container_add(GTK_CONTAINER(window), table);
-
-/*set atrributes */
-gtk_entry_set_alignment(GTK_ENTRY(entry), 1);
-gtk_editable_set_editable(GTK_EDITABLE(entry), FALSE);
-gtk_table_attach_defaults(GTK_TABLE(table), entry, 0, 4, 0, 1);
+static void cb_print_inc (GtkWidget *widget,gpointer data){
+ g_print ("%s=%i image[0]=%p<-%p\n",(char*)data,color,image[0],image[color]);
  
+ if (color<color_max) color++; else color=color_min;
+ image[0]=image[color];
+
+}
+
+static void cb_print_dec (GtkWidget *widget,gpointer data){
+ g_print ("%s=%i image[0]=%p<-%p\n",(char*)data,color,image[0],image[color]);
+
+ if (color>color_min) color--; else color=color_max;
+ image[0]=image[color];
+
+}
+
+static gboolean cb_expose_event (GtkWidget *widget, GdkEventExpose * event, gpointer data){
+ g_print ("%s=%i image[0]=%p<-%p\n",(char*)data,color,image[0],image[color]);
+
+ if (color>color_min) color--; else color=color_max;
+ image[0]=image[color];
+
+}
+
+
 int main (int argc, char *argv[])
 {
-    GtkWidget *window;
+  
+ gtk_init (&argc, &argv);
+ window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+ gtk_window_set_title (GTK_WINDOW (window), "LED-Test");
  
-    gtk_init (&argc, &argv);
+ g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
+ gtk_container_set_border_width (GTK_CONTAINER (window), 10);
  
-    /* gui main window´ */
-    window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title (GTK_WINDOW (window), "VIP-Club");
-    g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
+ list = gtk_list_box_new ();
+ gtk_container_add (GTK_CONTAINER (window), list);
  
-    gtk_widget_show_all(window);
-    gtk_main();
+ button = gtk_button_new_with_label ("prev color");
+ g_signal_connect (button, "clicked", G_CALLBACK (cb_print_dec), "prev color");
+ gtk_list_box_insert (GTK_LIST_BOX (list), button,0);
  
-    return 0;
+ button = gtk_button_new_with_label ("next color");
+ g_signal_connect (button, "clicked", G_CALLBACK (cb_print_inc), "next color");
+ gtk_list_box_insert (GTK_LIST_BOX (list), button,1);
+ 
+ button = gtk_button_new_with_label ("Quit");
+ g_signal_connect (button, "clicked", G_CALLBACK (gtk_main_quit), NULL);
+ gtk_list_box_insert (GTK_LIST_BOX (list), button,2);
+
+
+ image[0] = gtk_image_new_from_file ("led_gray.jpg");
+ image[1] = gtk_image_new_from_file ("led_red.jpg");
+ image[2] = gtk_image_new_from_file ("led_green.jpg");
+ image[3] = gtk_image_new_from_file ("led_blue.jpg");
+
+ g_signal_connect (G_OBJECT (image[0]), "expose_event",
+ G_CALLBACK (cb_expose_event), NULL);
+
+ gtk_list_box_insert (GTK_LIST_BOX (list), image[0],3);
+ gtk_widget_show_all (window);
+ gtk_main ();
+ return 0;
 }
--- develop/gui/vipclub-gui.h
+++ develop/gui/vipclub-gui.h
@@ -1,1 +1,2 @@
-#include "../config.h"
+#include "../src/config.h"
+
