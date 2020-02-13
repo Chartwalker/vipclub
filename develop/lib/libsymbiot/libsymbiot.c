b6188272c967e4c43ff0be76830897e2299abf40
Chartwalker <bastelmaus@chartwalker.net>
2020-02-14 00:57:38 +0100

    Signed-off-by: Chartwalker <bastelmaus@chartwalker.net>

---
+++ develop/lib/libsymbiot/libsymbiot.c
@@ -0,0 +1,19 @@
+# 13.2.2020 MIT C. by Tarnsat (ukn)
+#include <lib/libsymbiot/libsybbiot.h>
+
+typedef struct{
+    char *tb_data_p;
+    FILE *tp_fp;
+} libsymbiot_t;
+
+int tb_libsymbiot( libsymbiot_t *data_p){
+    if (data_p) {
+	fprintf{tp_fp,"p->=%p"}
+    } 
+    else 
+    {
+	fprintf{tp_fp,"p->=%p (nil)"}
+    };
+    
+    return 0;
+}
