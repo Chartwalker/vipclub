a790caedbe767f54d62e9caa214a8c6dfe139ba2
bastelmaus@chartwalker.net <bastelmaus@chartwalker.net>
2020-10-04 22:36:46 +0200

    debug minir changes

--- develop/src/lib/libsymbiot/tb_libsymbiot.c
+++ develop/src/lib/libsymbiot/tb_libsymbiot.c
@@ -48,7 +48,7 @@ int tb_libsymbiot_dot( Libsymbiot_Conf_t *Dot_p){
 
 int main(int argc, char **arv){
 
-	FILE *debug_fp;
+	FILE *log_fp;
 	FILE *dot_fp;
 	static int *retval_i;
 	static char *retval_s;
@@ -83,14 +83,15 @@ int main(int argc, char **arv){
 	Debug_Conf_p=&Debug_Dot;
 	Buffer.buf_p=hexbuf;
 	
-	 if ((debug_fp=fopen(TBLOG,"w+"))==NULL){
+	 if ((log_fp=fopen(TBLOG,"w+"))==NULL){
 		fprintf(stderr,"main_tb: unable to open %s\n",TBLOG);
     }
     else {
-		Root_Conf_p->log_fp=debug_fp;
-		fprintf(Root_Conf_p->log_fp,"main_tb: env: %%p(Root_Conf_p)=%p  %%p(Debug_Conf_p)=%p\n",Root_Conf_p, Debug_Conf_p);	
+		Root_Conf_p->log_fp=log_fp;
+		Debug_Conf_p->log_fp=log_fp;
 		fprintf(Root_Conf_p->log_fp,"main_tb: env: PAGE_SIZE=%u, CHAR_BIT=%u\n", PAGE_SIZE, CHAR_BIT);	
-		fprintf(Root_Conf_p->log_fp,"main_tb: env: %%p(Root_p)=%p sz=%i\n",Root_Conf_p, sizeof(*Root_Conf_p));
+		fprintf(Root_Conf_p->log_fp, "main_tb: log-test-root  %%p(Root_Conf_p)=%p sz=%i\n",Root_Conf_p,sizeof(*Root_Conf_p)); 
+		fprintf(Debug_Conf_p->log_fp,"main_tb: log-test-debug %%p(Debug_Conf_p)=%p sz=%i\n",Debug_Conf_p,sizeof(*Debug_Conf_p));	
 	}	
 	// create dot for navigation
 	 if ((dot_fp=fopen(TBDOT,"w+"))==NULL){
@@ -98,6 +99,7 @@ int main(int argc, char **arv){
     }
     else {
 		Root_Conf_p->dot_fp=dot_fp;
+		Debug_Conf_p->dot_fp=dot_fp;
 		fprintf(Root_Conf_p->log_fp,"main_tb: dot: dot_file=%s enabled",TBDOT);	
 	}	
 	// copy root für debugging
@@ -108,12 +110,12 @@ int main(int argc, char **arv){
 		Debug_Conf_p->log_fp=Root_Conf_p->log_fp;
 		Debug_Conf_p->dot_fp=Root_Conf_p->dot_fp;
 		fprintf(Debug_Conf_p->log_fp,"main_tb: logtest from Debug_Conf_p at %p\n",Debug_Conf_p);	
-		tb_libsymbiot_dot( Debug_Conf_p);
+	//	tb_libsymbiot_dot( Debug_Conf_p);
 //		fprintf(Debug_Conf_p->dot_fp,"\tstruct1 [share=record,label=\"<f0>| %%p(Root_p)=\n%p|<f1>%%p(Config_p)=\n%p | %%p(Dot_p)=\n%p\"];",);
 	}			
-	fprintf(Root_Conf_p->log_fp,"main_tb: try to create intelhex from %%p(Root_p)=%p\n",Root_Conf_p);
+	fprintf(Root_Conf_p->log_fp,"\nmain_tb: try to create intelhex from %%p(Root_p)=%p\n",Root_Conf_p);
 	libsymbiot_intelhex(Root_Conf_p);
-	fclose (debug_fp);
+	fclose (log_fp);
 	fclose (dot_fp);
     return 0;
 }
